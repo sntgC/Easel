09dfd10454ebbdc7983739e2f97d9b63f92cdcbb
Santiago <sntgcortes23@gmail.com>
2016-09-15 13:21:40 -0500

    Added save file infrastructure

---
+++ src/FileEditor/SaveFile.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FileEditor;
+
+import WorldGen.World;
+
+/**
+ *
+ * @author sntgc
+ */
+public class SaveFile {
+    public static boolean save(String location, World map){
+        return false;
+    }
+    
+    public static boolean load(String location, World map){
+        return false;
+    }
+}
